d500bb6b16b780af17f464107f826258de22c160
Richard Guay <raguay@customct.com>
2022-07-06 08:58:42 +0700

    Added the ability to set the name of the Alfred program.

--- README.md
+++ README.md
@@ -5,6 +5,8 @@ Vim. This extension will set the `normal` keymap command 'a' to open the current
 manager (a directory or a file) to Alfred to show in the browser. Then you can use Alfred to run 
 actions on that item.
 
+Before opening items in the Alfred Browser, you need to set the name of the Alfred program. When I wrote this, it was 'Alfred 4', but now there is 'Alfred 5'. So, you need to run the command "Set Alfred's Name" to set the proper name for the Alfred program.
+
 This extension is for macOS only since Alfred is only on macOS. This is also for the Wails 2 version of Modal File Manager.
 
 
--- main.js
+++ main.js
@@ -1,14 +1,39 @@
 var alfred = {
   extMan: null,
   fs: null,
-  init: function(extManager) {
+  alfredString: '',
+  alfredConfig: '',
+  init: async function(extManager) {
     //
     // This function adds all the commands for working with alfred and 
     // setting up references to variables that are used.
     //
     alfred.extMan = extManager;
     alfred.fs = extManager.getLocalFS();
+    let configDir = await alfred.fs.getConfigDir();
+    alfred.alfredConfig = `${configDir}/alfred.json`;
+    if(await alfred.fs.fileExists(alfred.alfredConfig)) {
+      alfred.alfredString = await alfred.fs.readFile(alfred.alfredConfig);
+      alfred.alfredString = JSON.parse(alfred.alfredString).alfredString;
+    } else {
+      alfred.alfredString = "Alfred 5";
+      await alfred.fs.writeFile(alfred.alfredConfig, JSON.stringify({
+        alfredString: alfred.alfredString
+      }));
+    }
     alfred.extMan.getCommands().addCommand('Open in Alfred', 'alfred.open', 'Open the current entry in the Alfred browser.', alfred.open);
+    alfred.extMan.getCommands().addCommand('Set Alfred\'s Name', 'alfred.setName', 'Set the name for the Alfred Application', alfred.setName);
+  },
+  setName: function() {
+    alfred.extMan.getExtCommand('askQuestion').command('Alfred', 'What is the name of the Alfred program?', async (result) => {
+      result = result.trim();
+      if(result !== '') {
+        alfred.alfredString = result;
+        await alfred.fs.writeFile(alfred.alfredConfig, JSON.stringify({
+          alfredString: alfred.alfredString
+        }));
+      }
+    });
   },
   installKeyMaps: function() {
     alfred.extMan.getExtCommand('addKeyboardShort').command('normal', false, false, false, 'a', alfred.open);
@@ -24,7 +49,8 @@ var alfred = {
     //
     // Use AppleScript command line to open the cursor in Alfred.
     //
-    await alfred.fs.runCommandLine('osascript -e \'tell application "Alfred 4" to browse "' + cursor.entry.dir + '/' + cursor.entry.name + '" \'');
+    console.log(`osascript -e 'tell application "${alfred.alfredString}" to browse "${cursor.entry.dir}/${cursor.entry.name}"'`);
+    await alfred.fs.runCommandLine(`osascript -e 'tell application "${alfred.alfredString}" to browse "${cursor.entry.dir}/${cursor.entry.name}"'`);
   }
 };
 return (alfred);
